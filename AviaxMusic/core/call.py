aa78afc50ff5b38cf6323726e6e6c5c5905cb38a
ixbittu <shivamlkr628@gmail.com>
2026-02-01 23:14:37 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -30,7 +30,7 @@ from AviaxMusic.utils.formatters import check_duration, seconds_to_min, speed_co
 from AviaxMusic.utils.inline.play import stream_markup
 from AviaxMusic.utils.stream.autoclear import auto_clean
 from AviaxMusic.utils.thumbnails import get_thumb
-from strings import get_string
+from strings import gen_string
 
 autoend = {}
 counter = {}
@@ -523,3 +523,4 @@ class Call(PyTgCalls):
 
 
 Aviax = Call()
+
